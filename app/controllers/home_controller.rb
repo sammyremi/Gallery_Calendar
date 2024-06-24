27e009df81be63490d9b26e24b7fd93df60ff44b
sammyremi <adebayorsamuelas@gmail.com>
2024-06-24 22:58:29 +0100

    previous date fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,16 @@
+require "date"
 class HomeController < ApplicationController
   def index
+
   end
 
   def show
+    date = Date.today
+
+    @previous_month = date.prev_month
+    @temp = @previous_month
+    puts @temp.prev_month
+
+
   end
 end
