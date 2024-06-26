6958357b400be3593905cbf17aace426aa85368f
sammyremi <adebayorsamuelas@gmail.com>
2024-06-26 06:45:20 +0100

    fix current month color

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 require "date"
 class HomeController < ApplicationController
   def index
-
+    today_date = Time.now.to_s
+    @today_date = today_date.split(" ")[0]
+    @today = @today_date.split("-")[2].to_i
+    puts @today_date.class, @today_date
   end
 
   def show
