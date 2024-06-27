a8301798542b4f86161360b980909aff725ca62c
sammyremi <adebayorsamuelas@gmail.com>
2024-06-27 21:04:14 +0100

    add image to 3d gallery

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
     today_date = Time.now.to_s
     @today_date = today_date.split(" ")[0]
     @today = @today_date.split("-")[2].to_i
-    puts @today_date.class, @today_date
+    puts @today
   end
 
   def show
